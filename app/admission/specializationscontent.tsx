169212ddaac09983b677cc297bc2b0d7d049f284
vinoth <vinor1213@gmail.com>
2025-12-11 18:22:39 +0530

    push

--- app/admission/specializationscontent.tsx
+++ app/admission/specializationscontent.tsx
@@ -311,7 +311,7 @@ export default function SpecializationPageContent({ activeSlug }: any) {
                                         xmlns="http://www.w3.org/2000/svg"
                                     >
                                         <path
-                                            fill="#4A301C"
+                                            fill="#835d40ff"
                                             fillOpacity="0.4"
                                             d="M0,256L80,224C160,192,320,128,480,112C640,96,800,128,960,149.3C1120,171,1280,181,1360,186.7L1440,192L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
                                         ></path>
